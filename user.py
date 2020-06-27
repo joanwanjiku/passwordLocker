ccc996a178837528988d5099e1c585a068a3c1bc
Joan_E <joanevans18@gmail.com>
2020-06-27 23:15:27 +0300

    Added an __init__ and test_init method

--- user.py
+++ user.py
@@ -2,3 +2,9 @@ class User:
     """
     Describes the behaviours of the class user
     """
+    def __init__(self, name, password):
+        """
+
+        """
+        self.name = name
+        self.password = password
--- user_test.py
+++ user_test.py
@@ -1,7 +1,22 @@
 import unittest
 from user import User
 
+
 class TestUser(unittest.TestCase):
     """
     All test cases for the class user
     """
+    def setUp(self):
+        """
+
+        """
+        self.new_user = User("Joan", "foobar")
+
+    def test_init(self):
+        """
+        tests whether the object has been initialized properly
+        """
+        self.assertEqual(self.new_user.name, "Joan")
+        self.assertEqual(self.new_user.password, "foobar")
+
+
