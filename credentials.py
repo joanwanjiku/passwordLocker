a52f8235484c821538eeda3e08e59db3aee6db14
Joan_E <joanevans18@gmail.com>
2020-06-27 23:21:25 +0300

    Added object methods

--- credentials.py
+++ credentials.py
@@ -2,3 +2,48 @@ class Credentials:
     """
     Describes the attributes and methods of the class credentials
     """
+    credential_list = []
+
+    def __init__(self, app, user, password):
+        """
+
+        :param app:
+        :param user:
+        :param password:
+        """
+        self.app_name = app
+        self.username = user
+        self.password = password
+
+    def save_credential(self):
+        """
+
+        :return:
+        """
+        Credentials.credential_list.append(self)
+
+    def delete_credential(self):
+        """
+
+        :return:
+        """
+        Credentials.credential_list.remove(self)
+
+    @classmethod
+    def find_by_app_name(cls, name):
+        """
+
+        :param name: appname
+        :return: credentials
+        """
+        for credentials in cls.credential_list:
+            if credentials.app_name == name:
+                return credentials
+
+    @classmethod
+    def display_credentials(cls):
+        """
+
+        :return:
+        """
+        return cls.credential_list
